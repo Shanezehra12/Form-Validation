cc066ad93c4ebf73af5c6e1f337d81c5e316d04e
Shanezehra12 <112850728+Shanezehra12@users.noreply.github.com>
2023-09-12 17:24:12 +0500

    First Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Register from "./register";
+
+const App = () => {
+  return (
+    <main className="App">
+      <Register />
+    </main>
+  );
+};
+
+export default App;
